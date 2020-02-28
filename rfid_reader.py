c30631b21c00ddd4d9c9e3e967649711aa494f2b
Vladimir Scherbo <vscherbo@gmail.com>
2020-02-28 18:31:45 +0300

    write to file first; DB threading

--- rfid_reader.py
+++ rfid_reader.py
@@ -2,100 +2,217 @@
 """ RFID reader """
 
 import os
+import sys
+import logging
+import time
+import io
 from datetime import datetime
+import threading
 from evdev import InputDevice, categorize #, _ecodes
 from evdev.ecodes import EV_KEY
 from sig_app import Application
 from pg_app import PGapp
+import log_app
 
+RFID_NAME = 'RFID'
+DEV_DIR = '/dev/input'
 SQL_INSERT = """INSERT INTO rep.rfid_history(card_num) VALUES('{}');"""
 
-class RFIDReader(Application, PGapp):
+class StoppableThread(threading.Thread):
+    """Thread class with a stop() method. The thread itself has to check
+    regularly for the stopped() condition."""
+
+    def __init__(self, *args, **kwargs):
+        super(StoppableThread, self).__init__(*args, **kwargs)
+        self._stop_event = threading.Event()
+
+    def stop(self):
+        """ stop thread """
+        self._stop_event.set()
+
+    def stopped(self):
+        """ check if stopped """
+        return self._stop_event.is_set()
+
+
+class CSVWriter(PGapp):
+    """ Monitor csv dir and write found files to PG """
+    def __init__(self, pg_host, pg_user):
+        super(CSVWriter, self).__init__(pg_host, pg_user)
+        if self.pg_connect():
+            self.set_session(autocommit=True)
+        self.csv_list = []
+        self.do_loop = True
+
+    def chk_csv_dir(self, csv_dir, arch_dir, failed_dir, stop):
+        """ Read "csv_dir" everey chk_period and write found files to PG
+        """
+        csv_str = ''
+        chk_period = 5
+        logging.debug('listdir of %s', csv_dir)
+        while self.do_loop:
+            fcsv_list = os.listdir(csv_dir)
+            for fcsv in fcsv_list:
+                logging.debug('reading csv file %s', fcsv)
+                with open('{}/{}'.format(csv_dir, fcsv), 'r') as csv:
+                    csv_str = csv.readline()[:-1]
+                if csv_str:
+                    self.csv_list.append(csv_str)
+                    csv_str = ''
+            logging.info('Found: csv_list=%s', self.csv_list)
+            if self.csv_list:
+                csv_io = io.StringIO('\n'.join(self.csv_list))
+                if self.copy_from(csv_io, 'rep.rfid_history', sep='^', \
+                        columns=('card_num', 'dt_read')):
+                    # move csv to 99-archive
+                    for fcsv in fcsv_list:
+                        os.rename(fcsv, '{}/{}'.format(arch_dir, fcsv))
+                else:
+                    # move csv to 98-failed
+                    for fcsv in fcsv_list:
+                        os.rename(fcsv, '{}/{}'.format(failed_dir, fcsv))
+
+            time.sleep(chk_period)
+            self.do_loop = stop()
+
+    def _db_write(self):
+        """ write to table rep.rfid_history """
+        if not self.conn:
+            logging.info('DB not connected. Try to re-connect.')
+            if self.pg_connect():
+                self.set_session(autocommit=True)
+
+        card_num = ''  # read from 02-csv/*csv
+        if self.do_query(SQL_INSERT.format(card_num)):
+            logging.info('Saved to DB')
+
+class RFIDReader(Application, log_app.LogApp):
     """ RFID Reader loop app """
 
-    def __init__(self, pg_host, pg_user):
+    dev_id_dir = '%s/by-id' % DEV_DIR
+    def __init__(self, args):
         self.do_read_one = True
         self.card_num_list = []
-        self.c_ev = {}
+        self.postponed = False
+        log_app.LogApp.__init__(self, args=args)
+        script_name = os.path.splitext(os.path.basename(__file__))[0]
+        self.get_config('{}.conf'.format(script_name))
         super(RFIDReader, self).__init__()
-        PGapp.__init__(self, pg_host=pg_host, pg_user=pg_user)
+        self.csv_writer = CSVWriter(self.config['PG']['pg_host'],\
+                self.config['PG']['pg_user'])
+        self.reader = InputDevice(self.dev_file)
+        self.reader.grab()
+        if 'base_dir' in self.config['DIRS'].keys():
+            self.base_dir = self.config['DIRS']['base_dir']
+        else:
+            self.base_dir = os.path.dirname(__file__)
+
+    @property
+    def dev_file(self):
+        """ Find RFID reader in /dev/input """
+
+        for inp in os.listdir(self.dev_id_dir):
+            if RFID_NAME in inp:
+                dev_link = os.readlink('%s/%s' % (self.dev_id_dir, inp))
+                dev_file = '%s/%s' % (DEV_DIR, dev_link.replace('../', ''))
+                logging.info('RFID device found=%s', dev_file)
+                break
+        return dev_file
 
     def _signal_handler(self):
-        print('RFID signal_handler')
+        logging.info('RFID signal_handler')
         self.do_read_one = False
         super(RFIDReader, self)._signal_handler()
 
+
     def _write_card_num(self):
-        """ Write card_num to PG and csv """
+        """ Write card_num to CSV """
         card_num = ''.join(self.card_num_list)
-        print(card_num)
-        csv_str = '{}^{}'.format(card_num, datetime.utcnow())
-        if self.do_query(SQL_INSERT.format(card_num)):
-            csv_str += '^DB'
-        else:
-            print('DB Error')
-            #csv_str += '^CSV'
-        with open(RFID_CSV_FILE, 'a') as csv:
-            csv.write(csv_str + '\n')
+        logging.info('Try to save card_num=%s', card_num)
+        csv_str = '{}^{}'.format(card_num, datetime.now())
+
+        tmp_file = '{}/{}-{}.tmp'.format(self.config['DIRS']['tmp_dir'], int(time.time()), card_num)
+        with open(tmp_file, 'w') as tmp:
+            try:
+                tmp.write(csv_str + '\n')
+            except IOError as err:
+                logging.error('Cannot write to tmp_file. err=%s', err)
+            except:
+                logging.error("Unexpected error:%s", sys.exc_info()[0])
+                raise
+            else:
+                logging.info('Written to tmp:%s', tmp_file)
+                csv_file = '{}/{}.csv'.format(self.config['DIRS']['csv_dir'], \
+                        os.path.splitext(os.path.basename(tmp_file))[0])
+                os.rename(tmp_file, csv_file)
 
     def _proc_until_enter(self, arg_event):
         """ recognize pressed key """
         res = False
-        self.c_ev = categorize(arg_event)
-        if self.c_ev.keystate == 0:  # key UP
-            #print(c_ev, type(c_ev))
-            print('st={}, code={}'.format(self.c_ev.keystate, self.c_ev.keycode))
-            if self.c_ev.keycode != 'KEY_ENTER':  # and c_ev.keystate == 0:
-                self.card_num_list.append(self.c_ev.keycode.replace('KEY_', ''))
+        c_ev = categorize(arg_event)
+        if c_ev.keystate == 0:  # key UP
+            logging.debug('st=%s, code=%s', c_ev.keystate, c_ev.keycode)
+            if c_ev.keycode != 'KEY_ENTER':  # and c_ev.keystate == 0:
+                self.card_num_list.append(c_ev.keycode.replace('KEY_', ''))
             else:
-                print('ENTER detected. Exiting...')
+                logging.debug('ENTER detected. Exiting...')
                 res = True
         return res
 
+    @property
+    def _missed_dirs(self):
+        missed_dirs = []
+        for i_dir in self.config['DIRS'].values():
+            logging.debug('i_dir=%s', i_dir)
+            loc_dir = '{}/{}'.format(self.base_dir, i_dir)
+            if not os.path.exists(loc_dir):
+                logging.debug('missed loc_dir=%s', loc_dir)
+                missed_dirs.append(i_dir)
+        return missed_dirs
 
     def _main(self):
         """ Just main """
 
-        #print(READER.capabilities())
-        #cp = READER.capabilities(verbose=True)
-        #print(cp)
-        """
-        for cp_k, cp_v in CP[('EV_KEY', 1)]:
-            print(cp_k, cp_v)
-        """
+        missed_dirs = self._missed_dirs
+        if missed_dirs:
+            logging.error('Missed dirs: %s', missed_dirs)
+            self.terminated = True
+            th_csv = None
+        else:
+            #self.csv_writer.pg_connect()
+            th_csv = StoppableThread(target=self.csv_writer.chk_csv_dir, \
+                    kwargs={"csv_dir": self.config['DIRS']['csv_dir'],
+                            "arch_dir": self.config['DIRS']['arch_dir'],
+                            "failed_dir": self.config['DIRS']['failed_dir'],
+                            "stop": lambda: self.terminated})
+            th_csv.start()
+
         while not self.terminated:
             self.card_num_list = []
             #for event in READER.read_loop():
             self.do_read_one = True
             while self.do_read_one:
-                event = READER.read_one()
+                event = self.reader.read_one()
                 if event and event.type == EV_KEY:  # прочитано и KEY
                     if self._proc_until_enter(event):
                         self._write_card_num()
                         break
+        if th_csv:
+            th_csv.stop()
+            th_csv.join()
 
+    def close(self):
+        """ Close everything """
+        self.reader.ungrab()
+        self.reader.close()
+        self.csv_writer.pg_close()
+
+
+#        with open(self.config['FILES']['RFID_CSV_FILE'], 'a') as csv:
 
 
 if __name__ == '__main__':
-    # move to conf
-    RFID_NAME = 'RFID'
-    DEV_DIR = '/dev/input'
-    DEV_ID_DIR = '%s/by-id' % DEV_DIR
-    RFID_CSV_FILE = 'card_history.csv'
-
-    for inp in os.listdir(DEV_ID_DIR):
-        if RFID_NAME in inp:
-            dev_link = os.readlink('%s/%s' % (DEV_ID_DIR, inp))
-            dev_file = '%s/%s' % (DEV_DIR, dev_link.replace('../', ''))
-            #print(dev_file)
-
-    READER = InputDevice(dev_file)
-    READER.grab()
-
-    APP = RFIDReader(pg_host='vm-pg-devel.arc.world', pg_user='arc_energo')
-    APP.wait_pg_connect()
-    APP.set_session(autocommit=True)
+    ARGS = log_app.PARSER.parse_args()
+    APP = RFIDReader(args=ARGS)  #, pg_host='vm-pg-restore.arc.world', pg_user='arc_energo')
     APP.main_loop()
-
-    READER.ungrab()
-    READER.close()
+    APP.close()
